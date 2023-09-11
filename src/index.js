a57a93b3738c7b79a25a82441f999027ed1d69fa
CousinJoey <cousinjoeyllc@gmail.com>
2023-09-10 23:31:11 -0400

    passes props to app and allows intilization of application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { fetchMenu, fetchLogo, fetchFeatured, fetchRewards } from "./firebase";
+import {
+  fetchMenu,
+  fetchLogo,
+  fetchFeatured,
+  fetchRewards,
+  fetchCustomization,
+  fetchGiftCards,
+  addItemToCart,
+  fetchUserCart,
+  removeFromCart,
+  editCartItem,
+  clearCart,
+  getDefaultCard,
+  getCreditCardInfo,
+  getRewardsCardInfo,
+  getCardFunds,
+  addFundsRewardsCard,
+  removeFundsRewardsCard,
+  removeFundsDebitCard,
+  setDefaultCreditCard,
+  setDefaultRewardsCard,
+  addCartToHistory,
+  addToFavorites,
+  fetchUserFavorites,
+  removeFromFavorites,
+  fetchUserHistory,
+  addOrderToHistory,
+  fetchUserOrderHistory,
+  fetchStoreLocations,
+  fetchUserSelectedStore,
+  updateUserSelectedStore,
+} from "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -10,6 +41,9 @@ function Index() {
   const [logoItems, setLogoItems] = useState([]);
   const [featuredItems, setFeaturedItems] = useState([]);
   const [rewardItems, setRewardItems] = useState([]);
+  const [customizationData, setCustomizationData] = useState([]);
+  const [giftCards, setGiftCards] = useState([]);
+  const [storeLocations, setStoreLocations] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -18,19 +52,25 @@ function Index() {
       const logoData = await fetchLogo();
       const featuredData = await fetchFeatured();
       const rewardData = await fetchRewards();
+      const customizationData = await fetchCustomization();
+      const giftCardData = await fetchGiftCards();
+      const storeLocationsData = await fetchStoreLocations();
+      setStoreLocations(storeLocationsData);
+      setGiftCards(giftCardData);
       setMenuItems(menuData);
       setLogoItems(logoData);
       setFeaturedItems(featuredData);
       setIsLoading(false);
       setRewardItems(rewardData);
+      setCustomizationData(customizationData);
     }
     fetchData();
   }, []);
 
   if (isLoading) {
     return (
-      <div>
-        <p>Loading...</p>
+      <div className="loading-placeholder loading-centered">
+        <div className="loading-circle"></div>
       </div>
     );
   }
@@ -42,6 +82,32 @@ function Index() {
         logoItems={logoItems}
         featuredItems={featuredItems}
         rewardItems={rewardItems}
+        customizationData={customizationData}
+        giftCards={giftCards}
+        storeLocations={storeLocations}
+        addItemToCart={addItemToCart}
+        fetchUserCart={fetchUserCart}
+        removeFromCart={removeFromCart}
+        clearCart={clearCart}
+        editCartItem={editCartItem}
+        getDefaultCard={getDefaultCard}
+        getCreditCardInfo={getCreditCardInfo}
+        getRewardsCardInfo={getRewardsCardInfo}
+        getCardFunds={getCardFunds}
+        removeFundsDebitCard={removeFundsDebitCard}
+        addFundsRewardsCard={addFundsRewardsCard}
+        removeFundsRewardsCard={removeFundsRewardsCard}
+        setDefaultCreditCard={setDefaultCreditCard}
+        setDefaultRewardsCard={setDefaultRewardsCard}
+        addCartToHistory={addCartToHistory}
+        addToFavorites={addToFavorites}
+        fetchUserFavorites={fetchUserFavorites}
+        removeFromFavorites={removeFromFavorites}
+        fetchUserHistory={fetchUserHistory}
+        addOrderToHistory={addOrderToHistory}
+        fetchUserOrderHistory={fetchUserOrderHistory}
+        fetchUserSelectedStore={fetchUserSelectedStore}
+        updateUserSelectedStore={updateUserSelectedStore}
       />
     </React.StrictMode>
   );
